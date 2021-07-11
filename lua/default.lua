54c54847a171c55bb1c6ab09aebb85358617e50d
remy.yu <remy.yu@ucloud.cn>
2021-07-11 23:41:56 +0800

    lsp init

--- lua/default.lua
+++ lua/default.lua
@@ -1,11 +1,84 @@
-CONFIG_PATH = vim.fn.stdpath "config"
-DATA_PATH = vim.fn.stdpath "data"
-CACHE_PATH = vim.fn.stdpath "cache"
-TERMINAL = vim.fn.expand "$TERMINAL"
-
 O = {
     lang = {
         bash = {
+            enable = true
+        },
+        clojure = {
+            enable = false
+        },
+        cmake = {
+            enable = false
+        },
+        cpp = {
+            enable = false
+        },
+        csharp = {
+            enable = false
+        },
+        css = {
+            enable = true
+        },
+        dockerfile = {
+            enable = true
+        },
+        elixir = {
+            enable = false
+        },
+        go = {
+            enable = true
+        },
+        graphql = {
+            enable = false
+        },
+        haskell = {
+            enable = false
+        },
+        html = {
+            enable = true
+        },
+        java = {
+            enable = false
+        },
+        json = {
+            enable = true
+        },
+        kotlin = {
+            enable = false
+        },
+        latex = {
+            enable = true
+        },
+        lua = {
+            enable = true
+        },
+        php = {
+            enable = false
+        },
+        purescript = {
+            enable = false
+        },
+        python = {
+            enable = false
+        },
+        ruby = {
+            enable = false
+        },
+        rust = {
+            enable = false
+        },
+        svelte = {
+            enable = false
+        },
+        javascript = {
+            enable = true
+        },
+        vim = {
+            enable = true
+        },
+        vue = {
+            enable = false
+        },
+        yaml = {
             enable = true
         }
     },
