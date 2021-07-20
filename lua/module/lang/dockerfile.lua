7b5a25f017bbf99d083382044dd36470ad9bc4fb
yuchenyang1994 <yuchenyang1994@foxmail.com>
2021-07-20 11:56:39 +0800

    ConfigFile

--- lua/module/lang/dockerfile.lua
+++ lua/module/lang/dockerfile.lua
@@ -2,9 +2,13 @@ local M = {}
 
 M.setup = function()
     local lspconfig =  require("lspconfig")
-    lspconfig['dockerfile'].setup{}
+    lspconfig['dockerfile'].setup{
+        on_attach = M.on_attach
+    }
 end
 M.on_attach = function(client, bufnr)
+    require("module.lsp.format").setup(client, bufnr)
+    require("module.lsp.key").setup(client, bufnr)
 end
 
 return M
--- lua/module/lang/yaml.lua
+++ lua/module/lang/yaml.lua
@@ -4,6 +4,8 @@ M.setup = function()
 require("lspconfig")['yaml'].setup {}
 end
 M.on_attach = function(client, bufnr)
+    require("module.lsp.format").setup(client, bufnr)
+    require("module.lsp.key").setup(client, bufnr)
 end
 
 return M
