9ff5fddf4c1927f70980cf7cadc72295e0d47bb7
yuchenyang1994 <yuchenyang1994@foxmail.com>
2021-09-26 11:25:44 +0800

    Fix: nvim-tree update

--- lua/module/tree/init.lua
+++ lua/module/tree/init.lua
@@ -54,6 +54,7 @@ M.config = function()
 	  { key = "h", cb = tree_cb "close_node" },
 	  { key = "v", cb = tree_cb "vsplit" },
 	}
+    require'nvim-tree'.setup {}
   end
   local view_status_ok, view = pcall(require, "nvim-tree.view")
   if not view_status_ok then
