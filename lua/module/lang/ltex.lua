a11a394807d5b686394c1fd61f644b5a36d9f09e
yuchenyang1994 <yuchenyang1994@foxmail.com>
2021-11-19 17:28:00 +0800

    Fix: markdown

--- lua/module/lang/ltex.lua
+++ lua/module/lang/ltex.lua
@@ -1,6 +1,10 @@
-local M = {}
+local M = {
+    filetypes = { "bib", "markdown", "org", "plaintex", "rst", "rnoweb", "tex" },
+}
 
-M.keymap = function()
+M.on_attach = function(client, bufnr)
+    require("module.lsp.format").setup(client, bufnr)
+    require("module.lsp.key").setup(client, bufnr)
 end
 
 return M
